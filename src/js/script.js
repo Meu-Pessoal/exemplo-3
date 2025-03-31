0837f2010de4914f7396414efebd41971b3e5e62
Antonio Neto <antoniojacintoa@gmail.com>
2025-03-31 09:08:07 -0300

    Declaracoes e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//DECLARAÇÕES E VARIAVEIS
+
+var nome = 'fiap';
+console.log(nome);
+
+let idade = 19;
+console.log(idade);
+
+const sobrenome = 'Fiapinho';
+console.log(sobrenome);
+
+//DECLARANDO VARIAVEIS INDEFINIDAS
+let nome1;
+console.log(nome1);
+
+//DECLARAÇÃO DE VARIAVEL NULA
+let nome2 = null;
+console.log(nome2);
+
