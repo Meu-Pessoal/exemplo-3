3913650ac800b4dabcb170bb37875b495c81d9fd
Antonio Neto <antoniojacintoa@gmail.com>
2025-03-31 09:54:03 -0300

    Operadores aritmeticos/logicos/comparacao

--- src/js/script.js
+++ src/js/script.js
@@ -1,56 +1,77 @@
-//DECLARAÇÕES E VARIAVEIS
+// //DECLARAÇÕES E VARIAVEIS
 
-var nome = 'fiap';
-console.log(nome);
+// var nome = 'fiap';
+// console.log(nome);
 
-let idade = 19;
-console.log(idade);
+// let idade = 19;
+// console.log(idade);
 
-const sobrenome = 'Fiapinho';
-console.log(sobrenome);
+// const sobrenome = 'Fiapinho';
+// console.log(sobrenome);
 
-//DECLARANDO VARIAVEIS INDEFINIDAS
-let nome1;
-console.log(nome1);
+// //DECLARANDO VARIAVEIS INDEFINIDAS
+// let nome1;
+// console.log(nome1);
 
-//DECLARAÇÃO DE VARIAVEL NULA
-let nome2 = null;
-console.log(nome2);
+// //DECLARAÇÃO DE VARIAVEL NULA
+// let nome2 = null;
+// console.log(nome2);
 
-//DECLARAÇÕES E VARIAVEIS
-let exemplo1 = 10;
-console.log(typeof exemplo1);
+// //DECLARAÇÕES E VARIAVEIS
+// let exemplo1 = 10;
+// console.log(typeof exemplo1);
 
-let exemplo2 = 'Tecnologia';
-console.log(typeof exemplo2);
+// let exemplo2 = 'Tecnologia';
+// console.log(typeof exemplo2);
 
-let exemplo3 = true;
-console.log(typeof exemplo3);
+// let exemplo3 = true;
+// console.log(typeof exemplo3);
 
-let exemplo4 = {};
-console.log(typeof exemplo4);
+// let exemplo4 = {};
+// console.log(typeof exemplo4);
 
-let exemplo5 = [];
-console.log(typeof exemplo5);
+// let exemplo5 = [];
+// console.log(typeof exemplo5);
 
-//CONVERCAO DE VARIAVEIS
-let numFloat = 123.456;
-console.log(parseInt(numFloat));
+// //CONVERCAO DE VARIAVEIS
+// let numFloat = 123.456;
+// console.log(parseInt(numFloat));
 
-let numString = '123.456';
-console.log(parseFloat(numString));
+// let numString = '123.456';
+// console.log(parseFloat(numString));
 
 
-//METODOS
+// //METODOS
 
-//LENGTH = VERIFICA O TAMANHO DA STRING
-let frase = 'O mundo da tecnologia';
-console.log(frase.length);
+// //LENGTH = VERIFICA O TAMANHO DA STRING
+// let frase = 'O mundo da tecnologia';
+// console.log(frase.length);
 
-//INDEXOF = RETORNA UM TRECHO DE TEXTO
-let texto = 'A programação indomável e sustrntavel';
-console.log(texto.indexOf('ão'));
+// //INDEXOF = RETORNA UM TRECHO DE TEXTO
+// let texto = 'A programação indomável e sustrntavel';
+// console.log(texto.indexOf('ão'));
 
-//SLICE = RETORNA UM TRECHO DE TEXTO COM INICIO E FINAL
-let info = 'Processamento de ponta';
-console.log(info.slice(16, 25));
+// //SLICE = RETORNA UM TRECHO DE TEXTO COM INICIO E FINAL
+// let info = 'Processamento de ponta';
+// console.log(info.slice(16, 25));
+
+// //OPERADORES ARITIMETICOS
+// const num1 = 10;
+// const num2 = 20;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+//OPERADORES LOGICOS
+const num3 = 10;
+const num4 = 20;
+
+console.log(num3 < num4);
+console.log(num3 > num4 && num4 < num3);
+console.log(num3 < num4 ||  num4 < num3);
+
+//OPERADORES COMPARATIVOS
+console.log(num3 == num4);
+console.log(num3 === num4);
